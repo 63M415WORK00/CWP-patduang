2f1c7fb82bab05d7339f0480d36e6940fe0f5ed8
Yampatitta <72595778+63M415WORK00@users.noreply.github.com>
2026-09-21 16:25:56 +0700

    Add files via upload

---
+++ discovery_piscine/cell00/cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Patitta"
+last_name  = "Duangkaew"
+print(first_name, last_name)
---
+++ discovery_piscine/cell00/cell01/ex01/ex01.py
@@ -0,0 +1,4 @@
+first_name = "Patitta"
+last_name  = "Duangkaew"
+whole_name = first_name + " " + last_name
+print(whole_name)
